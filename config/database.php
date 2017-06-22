02cbb321ff7fe2c9e295f19721792cb56e0e9422
Levi Fuson <info@twitchinteract.com>
2017-06-22 11:29:56 -0500

    New DB Pass

--- config/database.php
+++ config/database.php
@@ -43,12 +43,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'unix_socket' => '/mnt/www/mysql/mysql.sock',
             'database' => env('DB_DATABASE', 'rauxa'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'password' => env('DB_PASSWORD', 'thaTe2AG'),
             //'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
