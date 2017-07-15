43efb44a0b6bdca3811a61ad835140f43d7548f6
Todd Lekan <todd.lekan@gmail.com>
2017-07-15 16:46:38 -0500

    docker

--- docker/prod/app-docker-entrypoint.sh
+++ docker/prod/app-docker-entrypoint.sh
@@ -2,7 +2,13 @@ chown -R www-data:www-data /var/www/storage
 chown -R mysql:mysql /var/www/storage/dbdata
 php artisan cache:clear
 php artisan config:clear
-php artisan migrate
 php artisan storage:link
+
+while ! mysqladmin ping -h database --silent; do
+    sleep 1
+done
+
+php artisan migrate
+
 php-fpm
 tail -f /dev/null
